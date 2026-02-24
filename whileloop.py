2282bb72802bdc2c06e595d94b84fdff5684ff38
GandechaGargi <gandechagargi@gmail.com>
2026-02-24 09:21:29 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,43 @@
+#print 1 to 10
+i=1
+while i<=10:
+    print(i)
+    i+=1
+#sum of natural numbers
+n=int(input("Enter n:"))
+i=1
+s=0
+while i <=n:
+    s=s+i
+    i=i+1
+    print("sum=",s)
+#table of  number
+num=int(input("Enter number:"))
+i=1
+while i<=10:
+    print(num , "x",i,"=",num*i)
+    i=i+1
+# print 1 to 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+#odd no. 1 and 20
+i = 1
+while i <= 20:
+    print(i)
+    i += 2
+#reverse 0 to 5
+i = 5
+while i >= 0:
+    print(i)
+    i -= 1
+#even numbers in 1 to 20
+i = 2
+while i <= 20:
+    print(i)
+    i += 2
+
+
+

9d0f81ea3555ddfb88836f9244ff8e001e3848b2
GandechaGargi <gandechagargi@gmail.com>
2026-03-07 08:16:41 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,37 @@
+from array import array
+arr=array('i',[10,20,30,40])
+print(len(arr))
+
+from array import array
+arr=array('i',[10,20,30])
+arr.append(40)
+print(arr)
+
+from array import array
+arr=array('i',[10,20,40])
+arr.insert(2,30)
+print(arr)
+
+from array import array
+arr=array('i',[10,20,30,20,40])
+arr.remove(20)
+print((arr))p
+
+from array import array
+arr=array('i',[10,20,30,40])
+x = arr.pop()
+print("removed :",x)
+print((arr))
+
+from array import array
+arr=array('i',[10,20,30,20,40])
+print(arr.index(30))
+
+from array import array
+arr=array('i',[10,20,30,20,40])
+print(arr.index(20))
+
+from array import array
+arr=array('i',[10,20,30,20,40])
+arr.reverse()
+print(arr)
